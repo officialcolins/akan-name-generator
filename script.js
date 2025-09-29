064ba8baba759df54c5cb089e9909342c4305956
officialcolins <colins079@gmail.com>
2025-09-29 16:47:04 +0300

    add

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.getElementById('akanForm').addEventListener('submit', function (e) {
   const birthDate = new Date(birthdateInput);
   const dayOfWeek = birthDate.getDay(); // 0 = Sunday, 6 = Saturday
 
-  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; 
   const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
